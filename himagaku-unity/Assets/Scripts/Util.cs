39c4b224ae2114e7f8afb7f014d6216b8c7db3b3
nunu-e64 <nunu-e64@github.com>
2016-06-18 19:04:06 +0900

    add Util Script

---
+++ himagaku-unity/Assets/Scripts/Util.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Util {
+
+	public static bool CompareTags(this Component _component, params string[] _tags) {
+		foreach (var tag in _tags) {
+			if (_component.CompareTag(tag)) return true;
+		}
+		return false;
+	}
+
+	public static T Choose<T>(int index, params T[] args)
+	{
+		if (index < 1 || index > args.Length)
+		{
+			return default(T);
+		}
+		else
+		{
+			return args[--index];
+		}
+	}
+}
