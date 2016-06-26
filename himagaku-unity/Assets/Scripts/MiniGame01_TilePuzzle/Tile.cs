9bd4d467e71efd57d581d3726b04060cc99f2166
nunu-e64 <nunu-e64@github.com>
2016-06-27 00:53:03 +0900

    create TilePuzzle Movement

---
+++ himagaku-unity/Assets/Scripts/MiniGame01_TilePuzzle/Tile.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MiniGame01_TilePuzzle {
+
+    public class Tile : MonoBehaviour {
+
+        public int col;
+        public int row;
+        private int trueCol, trueRow;
+        private Sprite sprite;
+
+        private Vector3 currentTransformPosition;
+        private const float Z_HOVER = -1.0f;
+        private bool iTweenMoving = true;
+        public bool isCorrectPosition { get; private set; }
+
+        public Tile Clone() {
+            Tile tile = new Tile();
+            tile.col = this.col;
+            tile.row = this.row;
+            tile.trueCol = this.trueCol;
+            tile.trueRow = this.trueRow;
+            tile.sprite = this.sprite;
+            tile.isCorrectPosition = this.isCorrectPosition;
+            return tile;
+        }
+
+        public void Initialize(int col, int row, int trueRow, int trueCol, Sprite sprite) {
+            this.col = col;
+            this.row = row;
+            this.trueCol = trueCol;
+            this.trueRow = trueRow;
+            this.sprite = sprite;
+            this.isCorrectPosition = (col == trueCol && row == trueRow);
+
+            this.GetComponent<SpriteRenderer>().sprite = sprite;
+            this.currentTransformPosition = this.GetTransformPosition(this.col, this.row, this.sprite);
+            this.transform.localPosition = this.currentTransformPosition;
+        }
+
+        public void ChangePosition(Vector2 tilePos) {
+            this.col = (int) tilePos.x;
+            this.row = (int) tilePos.y;
+            this.isCorrectPosition = (this.col == this.trueCol && this.row == this.trueRow);
+            this.currentTransformPosition = this.GetTransformPosition(this.col, this.row, this.sprite);
+
+            iTweenExtention.SerialPlay(
+                this.gameObject
+                , (iTweenAction)iTween.MoveTo, iTween.Hash("position", this.GetHoverPosition(), "time", 0.5f, "isLocal", true)
+                , (iTweenAction)iTween.MoveTo, iTween.Hash("position", this.currentTransformPosition, "time", 0.2f, "isLocal", true)
+            );
+        }
+
+        public void Selected() {
+            iTween.MoveTo(this.gameObject, iTween.Hash("position", this.GetHoverPosition(), "time", 0.3f, "isLocal", true));
+        }
+
+        public void Canceled() {
+            iTween.MoveTo(this.gameObject, iTween.Hash("position", this.currentTransformPosition, "time", 0.3f, "isLocal", true));
+        }
+
+        Vector2 GetTransformPosition(int tileCol, int tileRow, Sprite tileSprite) {
+            float x = tileCol * sprite.rect.width / tileSprite.pixelsPerUnit;
+            float y = tileRow * sprite.rect.height / tileSprite.pixelsPerUnit;
+            Debug.Log(tileCol + ":" + tileRow + " x:" + x + " y:" + y);
+            return new Vector2(x, y);
+        }
+
+        Vector3 GetHoverPosition() {
+            Vector3 hoverTargetPos = this.currentTransformPosition;
+            hoverTargetPos.z = Z_HOVER;
+            return hoverTargetPos;
+        }
+
+        public Vector2 GetTilePos() {
+            return new Vector2(this.col, this.row);
+        }
+    }
+}
--- himagaku-unity/Assets/Scripts/MiniGame01_TilePuzzle/TilePuzzleManager.cs
+++ himagaku-unity/Assets/Scripts/MiniGame01_TilePuzzle/TilePuzzleManager.cs
@@ -2,53 +2,82 @@
 using System.Collections;
 using System.Linq;
 
-public class TilePuzzleManager : MonoBehaviour {
+namespace MiniGame01_TilePuzzle {
+    public class TilePuzzleManager : MonoBehaviour {
 
-    [SerializeField]
-	private GameObject tile;
-    [SerializeField]
-    tileSpriteList[] tileSprites;
+        [SerializeField] private GameObject tilePrefab;
+        [SerializeField] private tileSpriteList[] tileSprites;
+        [System.Serializable] public class tileSpriteList {
+            public Sprite[] sprites;
+        }
 
-    [System.Serializable]
-    public class tileSpriteList {
-        public Sprite[] sprites;
+        private Tile firstTouchTile;
+
+        // タイルのスプライト作成とランダム初期配置
+        void Start() {
+            this.firstTouchTile  = null;
+
+            int rows = this.tileSprites.GetLength(0);
+            int cols = this.tileSprites[0].sprites.GetLength(0);
+            int[] ary = Enumerable.Range(0, rows * cols).Select(i => (int)i).ToArray();
+            Util.ShuffleIntArray(ary);
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    int rdm = ary[i * cols + j];
+                    GameObject newTile = Instantiate<GameObject>(this.tilePrefab);
+                    newTile.GetComponent<Tile>().Initialize(j, i, rdm / cols, rdm % cols, this.tileSprites[rdm / cols].sprites[rdm % cols]);
+                    newTile.transform.SetParent(this.transform);
+                }
+            }
+
+            // Set Panel Position
+            Sprite tmpSprite = this.tileSprites[0].sprites[0];
+            this.transform.position = -1 * new Vector2((cols - 1) * tmpSprite.rect.width, (rows - 1) * tmpSprite.rect.height) / 2 / tmpSprite.pixelsPerUnit;
+        }
+
+        // タップ判定
+        void Update() {
+            Vector3 touchPos;
+            if (!InputManager.Instance.GetTouchBeganPosition(out touchPos)) {
+                return;
+            }
+            Debug.Log(touchPos);
+            var touchedCollider = Physics2D.OverlapPoint(touchPos);
+            if (touchedCollider == null) {
+                return;
+            }
+            Tile touchedTile = touchedCollider.GetComponent<Tile>();
+            if (touchedTile != null) {
+                if (this.firstTouchTile == null) {
+                    this.SelectFirstTouchTile(touchedTile);
+                } else if (firstTouchTile != touchedTile) {
+                    this.ExchangeTile(firstTouchTile, touchedTile);
+                    this.firstTouchTile = null;
+                } else {
+                    this.CancelFirstTile();
+                }
+            }
+        }
+
+        // 一枚目選択
+        void SelectFirstTouchTile(Tile tile) {
+            this.firstTouchTile = tile;
+            this.firstTouchTile.Selected();
+        }
+
+        // 入れ替え動作
+        void ExchangeTile(Tile firstTile, Tile secondTile) {            
+            Debug.Assert(firstTile != null);
+            Debug.Assert(secondTile != null);
+            Vector2 temp = firstTile.GetTilePos();
+            firstTile.ChangePosition(secondTile.GetTilePos());
+            secondTile.ChangePosition(temp);
+        }
+
+        // 一枚目選択解除
+        void CancelFirstTile() {
+            this.firstTouchTile.Canceled();
+            this.firstTouchTile = null;
+        }
     }
-
-    // Use this for initialization
-    void Start() {
-        // Create Tile Sprite
-        int rows = tileSprites.GetLength(0);
-        int cols = tileSprites[0].sprites.GetLength(0);
-        int[] ary = Enumerable.Range(0, rows*cols).Select(i => (int)i).ToArray();
-        Shuffle(ary);
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols; j++) {
-                int rdm = ary[i * cols + j];
-                GameObject newTile = Instantiate<GameObject>(tile);
-                newTile.GetComponent<PuzzleTile>().Initialize(i, j, rdm/cols, rdm%cols, tileSprites[rdm/cols].sprites[rdm%cols]);
-                newTile.transform.SetParent(this.transform);
-			}
-		}
-
-        // Set Panel Position
-        Sprite tmpSprite = tileSprites[0].sprites[0];
-        this.transform.position = -1 * new Vector2((cols - 1) * tmpSprite.rect.width, (rows - 1) * tmpSprite.rect.height) / 2 / tmpSprite.pixelsPerUnit;
-	}
-
-	void Shuffle(int[] ary) {
-		System.Random rng = new System.Random();
-		int n = ary.Length;
-		while (n > 1) {
-			n--;
-			int k = rng.Next(n + 1);
-			int tmp = ary[k];
-			ary[k] = ary[n];
-			ary[n] = tmp;
-		}
-	}
-
-	// Update is called once per frame
-	void Update () {
-
-	}
 }
