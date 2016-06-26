3b0a794f3e251dc72b2d7e5599f1f1be0a8f7422
伊藤栄治 <eitoh@itoueiji-no-MacBook-Pro.local>
2016-06-26 22:29:53 +0900

    touch to player move

---
+++ himagaku-unity/Assets/Scripts/MiniGame02_BreadCompetition/Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Vector3 nextPosition;
+
+	// Use this for initialization
+	void Start () {
+		nextPosition = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = Vector3.MoveTowards(this.transform.position, nextPosition, 1.0f);
+	}
+
+	public void Move(Vector3 movement) {
+		Vector3 screenPos = Camera.main.WorldToScreenPoint(nextPosition);
+		screenPos += movement;
+		this.nextPosition = Camera.main.ScreenToWorldPoint(screenPos);
+	}
+
+
+	public float GetScreenPointLeftX() {
+		Vector3 worldLeft = this.transform.position; 
+		worldLeft.x -= this.transform.localScale.x / 2;
+
+		Vector3 screenLeft = Camera.main.WorldToScreenPoint(worldLeft);
+		return screenLeft.x;
+	}
+
+	public float GetScreenPointRightX() {
+		Vector3 worldRight = this.transform.position;
+		worldRight.x += this.transform.localScale.x / 2;
+
+		Vector3 screenRight = Camera.main.WorldToScreenPoint(worldRight);
+		return screenRight.x;
+	}
+}
