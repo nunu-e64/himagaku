d1ce35cd30e69641c52d476fcf58bf339998fbd1
Fumiya Amano <fumiya.amano@gree.co.jp>
2016-07-06 01:43:33 +0900

    temp

--- himagaku-unity/Assets/Scripts/iTweenFade.cs
+++ himagaku-unity/Assets/Scripts/iTweenFade.cs
@@ -4,10 +4,11 @@ using System.Collections;
 public class iTweenFade : MonoBehaviour {
 
     public static void Fade(GameObject target, float from, float to, float time) {
-        iTween.ValueTo(target, iTween.Hash('onupdate', setColor, 'parameter', 'from', from, 'to', to, 'time', time));
-                       
+		//iTween.ValueTo(target, iTween.Hash("onupdate", SetAlpha, "from", from, "to", to, "time", time));                       
     }
 
-    private void SetAlpha(float alpha) {
+	private void SetAlpha(float alpha) {
+		
+	}
         
 }
