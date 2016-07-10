5afb87ff23e53266ae23a44af85520a0e6bff680
Fumiya Amano <fumiya.amano@gree.co.jp>
2016-07-10 12:50:27 +0900

    improve iTweenFade to iTweenExt and create opening

--- himagaku-unity/Assets/Scripts/MiniGame01_TilePuzzle/TilePuzzleManager.cs
+++ himagaku-unity/Assets/Scripts/MiniGame01_TilePuzzle/TilePuzzleManager.cs
@@ -7,7 +7,9 @@ namespace MiniGame01_TilePuzzle {
     public class TilePuzzleManager : MonoBehaviour {
 
         [SerializeField] private GameObject tilePrefab;
+        [SerializeField] private Text timeText;
         [SerializeField] private Text clearText;
+        [SerializeField] private Text startText;
         [SerializeField] private GameObject backGround;
         [SerializeField] private GameObject filter;
         [SerializeField] private GameObject frame;
@@ -24,9 +26,12 @@ namespace MiniGame01_TilePuzzle {
             this.firstTouchTile = null;
             this.isClear = false;
             this.clearText.gameObject.SetActive (false);
+            this.timeText.gameObject.SetActive (false);
+            this.startText.gameObject.SetActive (false);
             this.Opening ();
         }
 
+        // パネルをシャッフルして配置
         [ContextMenu("InitializeTile")]
         void InitializePanels() {
             int rows = this.tileSprites.GetLength(0);
@@ -47,15 +52,37 @@ namespace MiniGame01_TilePuzzle {
             //Sprite tmpSprite = this.tileSprites[0].sprites[0];
             //this.transform.position = new Vector2(-1 * (cols - 1) * tmpSprite.rect.width, (rows - 1) * tmpSprite.rect.height) / 2 / tmpSprite.pixelsPerUnit;
 
-            this.CheckClear();
+            // Animation
+            iTweenExtention.SerialPlay (
+                this.gameObject,
+                (iTweenAction)iTween.MoveTo, this.frame, iTween.Hash("position", Vector3.zero, "time", 0.0f),
+                (iTweenAction)iTween.ScaleFrom, this.frame, iTween.Hash("scale", new Vector3 (7, 7, 7), "time",  1.0f, "oncomplete", "StartAnimation", "oncompletetarget", this.gameObject)
+            );
+            this.timeText.gameObject.SetActive (true);
+            iTween.MoveFrom(this.timeText.gameObject, iTween.Hash("y", 300, "time", 1.0f, "islocal", true));
         }
 
         void Opening() {
             iTweenExtention.SerialPlay (
                 this.gameObject,
-                (iTweenAction)iTweenSprite.FadeTo, this.filter, iTween.Hash ("from", 1.0f, "to", 0.0f, "time", 1.0f),
-                (iTweenAction)iTween.MoveTo, this.frame, iTween.Hash ("x", -10.0f, "time", 0.5f)
+                (iTweenAction) iTweenExt.FadeTo,  this.filter, iTween.Hash ("from", 1.0f, "to", 0.0f, "time", 1.0f),
+                (iTweenAction) iTween.MoveTo,        this.frame,  iTween.Hash ("x", -10.0f, "time", 1.0f, "oncomplete", "InitializePanels", "oncompletetarget", this.gameObject)
             );
+        }
+
+        void StartAnimation() {
+            this.startText.gameObject.SetActive (true);
+            iTweenExtention.SerialPlay (
+                this.gameObject,
+                (iTweenAction)iTweenExt.FadeTo, this.startText, iTween.Hash ("from", 0, "to", 1, "time", 0.5f),
+                (iTweenAction)iTweenExt.FadeTo, this.startText, iTween.Hash ("from", 1, "to", 0, "time", 0.5f)
+            );
+            iTweenExtention.SerialPlay (
+                this.gameObject,
+                (iTweenAction)iTween.MoveFrom, this.startText, iTween.Hash ("x", -100, "time", 0.5f),
+                (iTweenAction)iTween.MoveTo, this.startText, iTween.Hash ("x", 100, "time", 0.5f)
+            );
+            this.CheckClear();
         }
 
         // タップ判定
