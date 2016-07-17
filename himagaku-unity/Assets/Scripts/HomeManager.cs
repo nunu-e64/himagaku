a98e5d59503fd9cd403d0d95d03bf49e97b79df3
Fumiya Amano <fumiya.amano@gree.co.jp>
2016-07-17 15:31:34 +0900

    create Home Scene

---
+++ himagaku-unity/Assets/Scripts/HomeManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HomeManager : MonoBehaviour {
+
+    [SerializeField] private GameObject filter; 
+
+    // Use this for initialization
+    void Start () {
+        iTweenExt.ColorTo(this.filter, iTween.Hash("from", new Color(0, 0, 0, 1.0f), "to", new Color(0, 0, 0, 0.0f), "time", 0.5f, "oncomplete", "FinishOpening"));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- himagaku-unity/Assets/Scripts/TitleManager.cs
+++ himagaku-unity/Assets/Scripts/TitleManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour {
 
@@ -9,7 +10,7 @@ public class TitleManager : MonoBehaviour {
     // Use this for initialization
 	void Start () {
         canGoNext = false;
-        iTweenExt.ColorTo(this.filter, iTween.Hash("from", new Color(0, 0, 0, 1.0f), "to", new Color(0, 0, 0, 0.0f), "time", 2.0f, "oncomplete", "FinishOpening"));
+        iTweenExt.ColorTo(this.filter, iTween.Hash("from", new Color(0, 0, 0, 1.0f), "to", new Color(0, 0, 0, 0.0f), "time", 2.0f, "oncomplete", "FinishOpening", "oncompletetarget", this.gameObject));
 	}
 
     void FinishOpening () {
@@ -19,7 +20,11 @@ public class TitleManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (canGoNext && InputManager.Instance.GetTouchBegan()) {
-            //NEXT SCENE
+            iTweenExt.ColorTo(this.filter, iTween.Hash("from", new Color(0, 0, 0, 0.0f), "to", new Color(0, 0, 0, 1.0f), "time", 0.5f, "oncomplete", "GoNextScene", "oncompletetarget", this.gameObject));
         }
 	}
+
+    void GoNextScene () {
+        SceneManager.LoadScene("Home");
+    }
 }
