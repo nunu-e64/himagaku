13f1b1df874e73fb5d18cddc04416ef6e8c7dc39
Fumiya Amano <fumiya.amano@gree.co.jp>
2016-07-24 21:09:55 +0900

    add title label

--- himagaku-unity/Assets/Scripts/HomeManager.cs
+++ himagaku-unity/Assets/Scripts/HomeManager.cs
@@ -7,7 +7,7 @@ public class HomeManager : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        iTweenExt.ColorTo(this.filter, iTween.Hash("from", new Color(0, 0, 0, 1.0f), "to", new Color(0, 0, 0, 0.0f), "time", 0.5f, "oncomplete", "FinishOpening"));
+        iTweenExt.ColorTo(this.filter, iTween.Hash("from", new Color(0, 0, 0, 1.0f), "to", new Color(0, 0, 0, 0.0f), "time", 0.2f, "oncomplete", "FinishOpening"));
     }
 	
 	// Update is called once per frame
--- himagaku-unity/Assets/Scripts/TitleManager.cs
+++ himagaku-unity/Assets/Scripts/TitleManager.cs
@@ -5,16 +5,19 @@ using UnityEngine.SceneManagement;
 public class TitleManager : MonoBehaviour {
 
     [SerializeField] private GameObject filter; 
+    [SerializeField] private GameObject StartLabel;
     private bool canGoNext;
 
     // Use this for initialization
 	void Start () {
         canGoNext = false;
+        StartLabel.SetActive(false);
         iTweenExt.ColorTo(this.filter, iTween.Hash("from", new Color(0, 0, 0, 1.0f), "to", new Color(0, 0, 0, 0.0f), "time", 2.0f, "oncomplete", "FinishOpening", "oncompletetarget", this.gameObject));
 	}
 
     void FinishOpening () {
         canGoNext = true;
+        StartLabel.SetActive(true);
     }
 	
 	// Update is called once per frame
